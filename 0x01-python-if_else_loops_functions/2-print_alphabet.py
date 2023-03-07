491cb8530365f888c9220d5356600b7d6a429da0
AminatRazaq <aminatrazaq@yahoo.com>
2023-03-07 14:20:48 -0800

    task 3

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-#97 to 122 is for lowercase
-#recall the lastgit in range function is excluded
-#"chr" returns the character that reps the specified unicode.
 for alp in range(97, 123):
     print("{}".format(chr(alp)), end="")
