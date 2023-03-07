0d9491472e6dd738d8f72a2e0e04d44cd1117bf6
AminatRazaq <aminatrazaq@yahoo.com>
2023-03-07 15:50:18 -0800

    task 7

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,6 +2,6 @@
 for num in range(0, 10):
     for dec in range(num + 1, 10):
         if num == 8 and dec == 9:
-            print("{}{}".format(num,dec))
+            print("{}{}".format(num, dec))
         else:
-            print("{}{}".format(num,dec), end=", ")
+            print("{}{}".format(num, dec), end=", ")
