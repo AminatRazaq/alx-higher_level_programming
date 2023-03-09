91c10527079da84a15dd867e0a34259a25c21cd2
AminatRazaq <aminatrazaq@yahoo.com>
2023-03-09 10:03:12 -0800

    task 0

---
+++ 0x02-python-import_modules/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """Prints the sum of two numbers"""
+from add_0 import add
+a = 1
+b = 2
+"""To print 1+2=3"""
+print("{} + {} = {}".format(a, b, add(a, b)))
---
+++ 0x02-python-import_modules/README.md
@@ -0,0 +1,2 @@
+Ameenah, you are welcome to another edition of Python programming.
+Python - import & modules
