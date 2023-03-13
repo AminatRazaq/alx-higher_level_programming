192b79e22c9fec30d49f50efd308b12eecac9fa1
AminatRazaq <aminatrazaq@yahoo.com>
2023-03-13 12:31:30 -0700

    m8

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -9,8 +9,8 @@ def add_tuple(tuple_a=(), tuple_b=()):
             tuple_a = tuple_a[0], 0
     """if tuple_a is less than 2, so the no of elements is 0 or 1"""
     if len(tuple_b) < 2:
-        if (tuple_b[0]) == 0:
-            len(tuple_b) = 0, 0
+        if len(tuple_b) == 0:
+            tuple_b = 0, 0
         else:
             tuple_b = tuple_b[0], 0
     return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
