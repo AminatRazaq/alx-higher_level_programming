058d0727a38a4baa97192a813fd873d3d41bffe6
AminatRazaq <aminatrazaq@yahoo.com>
2023-03-13 10:52:33 -0700

    m5

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -5,8 +5,8 @@ def new_in_list(my_list, idx, element):
     at a specific position without modifying the original list"""
     if idx < 0 or idx > (len(my_list) - 1):
         return (my_list)
-    copy _list = [x for x in my_list]
+    copy = [x for x in my_list]
     """"X for X is to copy a list, see this link:
     'https://www.youtube.com/watch?v=v0UCdsOn4KM'"""
-    copy_list[idx] = element
-    return (copy_list)
+    copy[idx] = element
+    return (copy)
