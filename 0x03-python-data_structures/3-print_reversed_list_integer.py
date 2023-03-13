6a1efdb99af9926e312c845a2752f7ccba036473
AminatRazaq <aminatrazaq@yahoo.com>
2023-03-13 10:41:13 -0700

    m4

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,7 +3,8 @@
 def print_reversed_list_integer(my_list=[]):
     """a function that prints all integers
     of a list, in reverse order"""
+    if isinstance(my_list, list):
     my_list.reverse()
 
-    for i in range(len(my_list)):
-        print("{:d}".format(my_list[i]))
+    for i in my_list:
+        print("{:d}".format(i))
---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def new_in_list(my_list, idx, element):
+    """a function that replaces an element in a list
+    at a specific position without modifying the original list"""
+    if idx < 0 or idx > (len(my_list) - 1):
+        return (my_list)
+    copy _list = [X for X in my_list]
+    """"X for X is to copy a list, see this link:
+    'https://www.youtube.com/watch?v=v0UCdsOn4KM'"""
+    copy_list[idx] = element
+    return (copy_list)
