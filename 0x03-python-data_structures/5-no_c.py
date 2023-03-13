b3685af3864ce931139b2a0f4a53929c8d37807a
AminatRazaq <aminatrazaq@yahoo.com>
2023-03-13 11:51:34 -0700

    m7

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,10 +2,8 @@
 
 def no_c(my_string):
     """removes all characters c and C from a string"""
-
-
     """ join() returns a new string which is the concatenation
     of the other strings in the iterable specified"""
-    copy = [x for x in my_string if x != 'C' and x!= 'c']
+    copy = [x for x in my_string if x != 'C' and x != 'c']
     """"Below: the "" in return is to show the new empty string"""
     return ("".join(copy))
---
+++ 0x03-python-data_structures/a.c
@@ -0,0 +1,4 @@
+for i in range(R):
+    for j in range(C):
+        print(matrix[i][j], end = " ")
+    print()
