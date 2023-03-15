2803ad520be02fb1db74b770d9f0ba6ecbefceca
AminatRazaq <aminatrazaq@yahoo.com>
2023-03-15 14:48:15 -0700

    t5

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
-
 def number_keys(a_dictionary):
-    """returns the number of keys in a dictionary"""
-    list_of_keys = list(a_dictionary.keys())
-    num_keys = 0
-    for i in list_of_keys:
-        num_keys += 1
-
-    return (num_keys)
+    return len(a_dictionary.keys())
--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,8 +2,9 @@
 
 def print_sorted_dictionary(a_dictionary):
     """prints a dictionary by ordered keys"""
-    list_keys = list(a_dictionary.keys())
-    """after listing the keys, we now sort them alphabetically"""
-    list_keys.sort()
-    for i in list_keys:
-        print("{}: {}".format(i, a_dictionary.ge(i)))
+            print("{}: {}".format(i, a_dictionary.get(i)))
+#!/usr/bin/python3
+def print_sorted_dictionary(my_dict):
+    for k in sorted(my_dict.keys()):
+        print("{}: {}".format(k, my_dict[k]))
+
