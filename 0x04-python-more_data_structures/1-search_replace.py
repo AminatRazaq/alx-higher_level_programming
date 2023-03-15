fef897afb7c3228afbfa35399da653c25ef1580f
AminatRazaq <aminatrazaq@yahoo.com>
2023-03-15 12:24:29 -0700

    c1

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+    """replaces all occurrences of an
+    element by another in a new list"""
+    new_list = my_list.replace("search", "replace")
+    return (new_list)
+
