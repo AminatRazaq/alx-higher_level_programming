a0c7c06349165e1e2a737bb2d41714625f562788
AminatRazaq <aminatrazaq@yahoo.com>
2023-03-27 15:37:45 -0700

    p7

---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+def magic_calculation(a, b):
+    result_mag = 0
+    for i in range(1, 3):
+        try:
+            if i > a:
+                raise Exception('Too far')
+            else:
+                result_mag += a ** b / i
+        except:
+            result_mag = b + a
+            break
+    return (result_mag)
