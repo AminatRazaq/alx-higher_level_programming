da1adc474ac702999ae60f8c5ce2a92c0e475a5b
AminatRazaq <aminatrazaq@yahoo.com>
2023-03-27 15:52:09 -0700

    p7

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -9,6 +9,10 @@ def magic_calculation(a, b):
                 raise Exception('Too far')
             else:
                 result_mag += a ** b / i
+        """EXCEPTION can be used as a wildcard that catches (almost)
+        everything it is good practice to be as specific as possible
+        with the types of exceptions that we intend to handle, and
+        to allow any unexpected exceptions to propagate on"""
         except Exception:
             result_mag = b + a
             break
