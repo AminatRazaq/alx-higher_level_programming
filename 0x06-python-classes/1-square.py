db107604f13af3964ac73e650d9d8c04ed59c69e
AminatRazaq <aminatrazaq@yahoo.com>
2023-03-28 14:40:37 -0700

    y9

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+"""Defines a square"""
+
+class Square:
+    """Represents a size, with a size."""
+
+    def __init__(self, size):
+        """Ïnitializes the size"""
+        self.__size = size
