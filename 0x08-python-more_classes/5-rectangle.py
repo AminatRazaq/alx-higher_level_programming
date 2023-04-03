11a981191b82c953509caf4a2766b9d8a40b4e51
AminatRazaq <aminatrazaq@yahoo.com>
2023-04-03 15:21:52 -0700

    y6

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -69,3 +69,7 @@ class Rectangle:
         rect = "Rectangle(" + str(self.__width)
         rect += ", " + str(self.__height) + ")"
         return (rect)
+
+    def __del__(self):
+        """deletes an instance of "Rectangle" """
+        print("Bye rectangle...")
