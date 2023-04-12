5d6fdda8dead2d929f2a5c245dd7134c09a11a96
AminatRazaq <aminatrazaq@yahoo.com>
2023-04-11 18:47:46 -0700

    r2

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+"""Defines an Object"""
+
+
+def lookup(obj):
+    """returns the list of available attributes and methods of an object"""
+    return (dir(obj))
---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Defines a parent class-List and child class-MY List"""
+
+
+class MyList(list):
+    """displaying a parent and child class"""
+    
+    def print_sorted(self):
+        """prints the list, but sorted (ascending sort)"""
+        return (sorted(self))
