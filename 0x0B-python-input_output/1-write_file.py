69a9cfea563dd86613920785be04aa9f26da519f
AminatRazaq <aminatrazaq@yahoo.com>
2023-04-12 17:39:03 -0700

    j1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,7 +4,11 @@
 
 
 def write_file(filename="", text=""):
-    """writes a string to a text file (UTF8) and
-    returns the number of characters written"""
-    with open('filename', 'w' encoding="utf-8") as f:
-        return (f.write(text))
+    """writes a string to a text file (UTF8)
+    Args:
+        filename(str): the name of the file
+        text(str): text to wite the file 
+    Returns :
+        the number of characters written"""
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
