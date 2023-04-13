cae763b5404264a380d205cc4f708dd2ad17eac8
AminatRazaq <aminatrazaq@yahoo.com>
2023-04-12 18:18:01 -0700

    g6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+"""Defines a Create object from a JSON file"""
+
+
+def load_from_json_file(filename):
+    """creates an Object from a “JSON file”"""
+    with open(filename) as f:
+        return json.loads(f)
