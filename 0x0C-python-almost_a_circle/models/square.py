5ba7e71ea1ad942715a481a84b552f7739145a2c
AminatRazaq <aminatrazaq@yahoo.com>
2023-04-17 15:46:59 -0700

    q2

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -3,6 +3,7 @@
 """Defines class Square"""
 from models.rectangle import Rectangle
 
+
 class Square(Rectangle):
     """Represents a Square"""
 
@@ -63,3 +64,17 @@ class Square(Rectangle):
                     self.x = v
                 elif k == "y":
                     self.y = v
+
+    def to_dictionary(self):
+        """Returns dictionary representations of a square"""
+        return {
+            "id": self.id,
+            "size": self.width,
+            "x": self.x,
+            "y": self.y
+        }
+
+    def __str__(self):
+        """Return the print() and str() representation of a Square."""
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+                                                 self.width)
